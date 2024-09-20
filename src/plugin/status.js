22cde403847ba8ed2d718c2d7dad3730d26ac6e2
X.N.O.R Education <adithyashavinda2005@gmail.com>
2024-09-20 15:23:43 +0530

    Add files via upload

---
+++ src/plugin/status.js
@@ -0,0 +1,52 @@
+import fs from 'fs';
+import config from '../../config.cjs';
+
+const handleGreeting = async (m, gss) => {
+  try {
+    const textLower = m.body.toLowerCase();
+
+    const triggerWords = ["send", "share", "snd", "ewhan", "save", "ewana", "share", "ewanawako", "dpn", "save", "one", "danna", "dannako", "ewanna", "ewannako", "ewpn", "dapan", "danna","send", "share", "snd", "ewhan", "save", "ewana", "share", "ewanawako", "dpn", "save", "one", "danna", "dannako", "ewanna", "ewannako", "ewpn", "dapan", "danna", "oni"];
+
+    if (triggerWords.includes(textLower)) {
+      if (m.message && m.message.extendedTextMessage && m.message.extendedTextMessage.contextInfo) {
+        const quotedMessage = m.message.extendedTextMessage.contextInfo.quotedMessage;
+
+        if (quotedMessage) {
+          // Check if it's an image
+          if (quotedMessage.imageMessage) {
+            const imageCaption = quotedMessage.imageMessage.caption;
+            const imageUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.imageMessage);
+            await gss.sendMessage(m.from, {
+              image: { url: imageUrl },
+              caption: imageCaption,
+              contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 9999,
+                isForwarded: true,
+              },
+            });
+          }
+
+          // Check if it's a video
+          if (quotedMessage.videoMessage) {
+            const videoCaption = quotedMessage.videoMessage.caption;
+            const videoUrl = await gss.downloadAndSaveMediaMessage(quotedMessage.videoMessage);
+            await gss.sendMessage(m.from, {
+              video: { url: videoUrl },
+              caption: videoCaption,
+              contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 9999,
+                isForwarded: true,
+              },
+            });
+          }
+        }
+      }
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
+export default handleGreeting;
